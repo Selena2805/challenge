1bf8573b79942f53f27adc219c55397804db5c0d
selena <salihfaty1211@gmail.com>
2023-09-18 15:47:02 +0100

    algoritm

---
+++ algorithme/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    nombre = int(input("Entrez un nombre :"));
+    for i in range (1,11):
+            resultat = nomber*i
+    printf(nombre,"x",i,"=", resultat);
+
+    return 0;
+}
